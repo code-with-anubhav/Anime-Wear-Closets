1b5beb2c423a450837a803d8bc66621c41f68803
Anubhav <163306927+code-with-anubhav@users.noreply.github.com>
2024-12-10 12:32:29 +0530

    12th dec 2024 -added seperation of concern in model and created owner model

---
+++ config/mongoose-connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/anime_wear_closets")
+  .then(function () {
+    console.log("Database Connected"); // replace with debugers
+  })
+  .catch(function (err) {
+    console.error("Database isn't connected", err);
+  });
+
+  module.exports = mongoose.connection
